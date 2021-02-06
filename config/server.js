771f650f2093ce61cde84ca14971e0e73ded1e97
Sanzio Wesley <sanziowesley@hotmail.com>
2021-02-06 18:51:10 -0300

    Adjusting routes and views

--- config/server.js
+++ config/server.js
@@ -15,7 +15,7 @@ var app = express();
 
 /* setar as variaveis 'view engine' e 'views' do express */
 app.set('view engine', 'ejs');
-app.set('views', './app/vi');
+app.set('views', './app/views');
 
 /* configurar o middleware express.static */
 app.use(express.static('./app/public'));
