e02e3a6d30b74481ada277046bcc568b18831340
Sanzio Wesley <sanziowesley@hotmail.com>
2021-02-07 13:05:32 -0300

    Adjusting routes and controllers

--- app/routes/chat.js
+++ app/routes/chat.js
@@ -1,9 +1,9 @@
 module.exports = function (application) {
-  application.post('/', function (req, res) {
-    res.render("chat");
+  application.post('/chat', function (req, res) {
+    application.app.controllers.chat.iniciaChat(application, req, res)
   });
 
   application.get('/chat', function (req, res) {
-    res.render("chat");
+    application.app.controllers.chat.iniciaChat(application, req, res)
   });
 }
